bf8aa9bf1cf210272484e7325e0d2a150c238068
Kiet <kietpham99.kp@gmail.com>
2022-05-16 01:29:27 -0700

    Commit basic app server

---
+++ app_server/index.js
@@ -0,0 +1,150 @@
+const   express     = require('express'),
+	    bodyParser  = require('body-parser'),
+        path        = require('node:path');
+
+
+const cluster = require('cluster');
+
+const totalNumCPUs = require("os").cpus().length;
+
+const serverPort = 8080;
+
+
+if (cluster.isMaster) {
+    console.log(`Total num cpus: ${totalNumCPUs}`);
+    console.log(`Master pid is: ${process.pid}`);
+
+    let workers = [];
+
+    // Spawn worker at index 'i'.
+    let spawn = function(i) {
+        workers[i] = cluster.fork();
+
+        // If worker dies, spawn it again
+        workers[i].on('exit', function(code, signal) {
+            // console.log('respawning worker', i);
+            spawn(i);
+        });
+
+        // TODO: use this for IPC if needed
+        // // Check for new namespace message
+        // workers[i].on('message', function(msg) {
+        //     // Only intercept messages that have a newNamespaceEndpoint property
+        //     if (msg.newNamespaceEndpoint) {
+        //         console.log('Worker to master: ');
+        //         console.log(msg);
+
+        //         // Send to all workers the msg containing newNamespace
+        //         for (var j = 0; j < num_processes; j++) {
+        //             workers[j].send(msg);
+        //         }
+                
+        //     }
+        // });
+    };
+
+    // Spawn workers
+	for (var i = 0; i < totalNumCPUs; i++) {
+		spawn(i);
+	}
+} else {
+    console.log(`Worker with pid: ${process.pid} running`);
+
+    const app = express();
+    
+    // app.use statements go here
+
+    app.use(bodyParser.json()); // handle json data, needed for axios requests to put things in req.body
+	app.use(bodyParser.urlencoded({extended: true}));
+
+    app.set('views', path.join(__dirname, 'views'))
+    app.set('view engine', 'ejs')
+    
+    let userArr = [
+        {
+            id: "1",
+            username: 'FooUser',
+            title: 'Mr.'
+        },
+
+        {
+            id: "2",
+            username: 'BarUser',
+            title: 'Ms.'
+        },
+        {
+            id: "3",
+            username: 'FooBarUser',
+            title: 'Sir'
+        }
+    ];
+
+    app.get('/', (req, res) => {
+        console.log(`Worker ${process.pid} serving`);
+
+        res.send('<h1>ROOT ROUTE!</h1>');
+    });
+
+    app.get('/list-users', (req, res) => {
+        console.log(`Worker ${process.pid} serving`);
+
+        res.render('./index', { userArr });
+    });
+
+    app.get('/user-info/:userId', (req, res) => {
+        console.log(`Worker ${process.pid} serving`);
+
+        const { userId } = req.params;
+        const user = userArr.find(user => user.id === userId);
+        res.send(user);
+    });
+
+
+    app.get('/add-user', (req, res) => {
+        console.log(`Worker ${process.pid} serving`);
+
+        res.render('./add_user');
+    });
+
+    app.post('/add-user', (req, res) => {
+        console.log(`Worker ${process.pid} serving`);
+
+        const { title, username } = req.body;
+
+        const foundUser = userArr.find(user => user.username === username);
+        if (typeof foundUser === 'undefined') {
+            userArr.push({
+                id: userArr.length,
+                username: username,
+                title: title
+            });
+            res.redirect('/list-users');
+        } else {
+            res.send("ERROR: USER ALREADY EXISTS!");
+        }
+    });
+
+    app.get('/long-computation/:N', (req, res) => {
+        console.log(`Worker ${process.pid} serving`);
+
+        const { N } = req.params;
+
+        let sum = 0;
+
+        for (let i = 0; i < N; i++) {
+            for (let j = 0; j < N; j++) {
+                sum += i * j;
+            }
+        }
+
+        res.send({sum});
+    });
+
+    app.get('*', (req, res) => {
+        console.log(`Worker ${process.pid} serving`);
+
+        res.send('<h1>CATCH ALL ROUTE!</h1>');
+    });
+
+    app.listen(serverPort);
+}
---
+++ curl_request_computations.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done1" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done2" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done3" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done4" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done5" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done6" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done7" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done8" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done9" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done10" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done11" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done12" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done13" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done14" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done15" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done16" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done17" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done18" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done19" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done20" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done21" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done22" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done23" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done24" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done25" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done26" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done27" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done28" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done29" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done30" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done31" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done32" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done33" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done34" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done35" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done36" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done37" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done38" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done39" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done40" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done41" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done42" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done43" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done44" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done45" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done46" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done47" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done48" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done49" &
+curl -s http://127.0.0.1:8080/long-computation/5000 && echo "done50" &
+
+
+wait
