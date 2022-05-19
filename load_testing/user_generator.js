8877c1bb6c140efea7e1f4b3568b8dd54e0c925f
李季澄 <b05702057@ntu.edu.com>
2022-05-19 10:19:02 -0700

    distribution function

--- load_testing/user_generator.js
+++ load_testing/user_generator.js
@@ -7,32 +7,45 @@ function getRandomInt(max) {
 }
 
 function generatePhaseElements(phases, phase_idx, sample_lists, element_list) {
-    cur_phase = phases[phase_idx]
-    request_num = cur_phase.duration * cur_phase.arrivalRate
-    sample_list = sample_lists[phase_idx]
+    cur_phase = phases[phase_idx];
+    request_num = cur_phase.duration * cur_phase.arrivalRate;
+    sample_list = sample_lists[phase_idx];
     for (let i = 0; i < request_num; i++) {
-        idx = getRandomInt(sample_list.length)
+        idx = getRandomInt(sample_list.length);
         element_list.push(sample_list[idx]);
     }
-    return phase_idx + 1
+    return phase_idx + 1;
+}
+
+function generatePhasePattern(hot, max, num) {
+    sample_list = []
+    for (let i = 0; i < num; i++) {
+        get_hot = getRandomInt(2);
+        if (get_hot === 1) {
+            sample_list.push(hot.toString());
+        } else {
+            sample_list.push((getRandomInt(max) + 1).toString());
+        }
+    }
+    return sample_list;
 }
 
 // define load patterns
-element_list = [] // output
-sample_list1 = ["1", "1", "1", "1", "1", "1", "1", "1", "1", "2"] // user1 is hot
-sample_list2 = ["1", "2", "2", "2", "2", "2", "2", "2", "2", "2"] // user2 is hot
-sample_lists = [sample_list1, sample_list2, sample_list1] // modify this list to customize the load for each phase
+element_list = []; // output
+sample_list1 = generatePhasePattern(1, 3, 100); // user1 is hot
+sample_list2 = generatePhasePattern(2, 3, 100); // user2 is hot
+sample_lists = [sample_list1, sample_list2, sample_list1]; // modify this list to customize the load for each phase
 
 // parse the yaml file to get the parameters
 const doc = yaml.load(fs.readFileSync('customized_test.yml', 'utf8'));
 phases = doc.config.phases;
-phase_idx = 0
+phase_idx = 0;
 
 // generate elements for each phase
-phase_idx = generatePhaseElements(phases, phase_idx, sample_lists, element_list) // phase 0
-phase_idx = generatePhaseElements(phases, phase_idx, sample_lists, element_list) // phase 1
-phase_idx = generatePhaseElements(phases, phase_idx, sample_lists, element_list) // phase 2
+phase_idx = generatePhaseElements(phases, phase_idx, sample_lists, element_list); // phase 0
+phase_idx = generatePhaseElements(phases, phase_idx, sample_lists, element_list); // phase 1
+phase_idx = generatePhaseElements(phases, phase_idx, sample_lists, element_list); // phase 2
 
 // write the elements
-elements = element_list.join("\n")
+elements = element_list.join("\n");
 fs.writeFileSync('users.csv', elements);
