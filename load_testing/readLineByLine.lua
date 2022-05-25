ca78fb0d698e2f53672fe30b41a4ef4fe5150b2b
Kiet <kietpham99.kp@gmail.com>
2022-05-24 22:28:56 -0700

    Fix addition of num requests and response per thread lua script

--- load_testing/readLineByLine.lua
+++ load_testing/readLineByLine.lua
@@ -12,6 +12,8 @@ local threadIdxCounter = 0
 
 local file_handle = io.open( "users.csv" ) 
 
+local threads = {} -- For each thread to keep track of num requests and responses
+
 -- Called at the beginning to initialize global context for all threads. Avoid duplicating work
 -- across threads here
 function setup(thread)
@@ -33,7 +35,12 @@ function setup(thread)
         append("172.23.144.1", 3001)
     end
     
+    print("all addrs:")
     for i,v in ipairs(addrs) do print(i,v) end  -- print addrs
+
+    -- TODO testing
+    thread:set("id", threadIdxCounter)
+    table.insert(threads, thread)
 
     local addrIndex = threadIdxCounter % #addrs + 1  -- since lua data structures are 1-indexed, mod then + 1. mod to wrap around
     threadIdxCounter = threadIdxCounter + 1
@@ -43,6 +50,10 @@ function setup(thread)
 end
 
 function init(args)
+    -- Initialize fields to keep track of num requests and responses
+    numRequests = 0
+    numResponses = 0
+
     local msg = "thread addr: %s"
     print(msg:format(wrk.thread.addr))
     -- -- Print members of wrk.thread
@@ -62,8 +73,29 @@ request = function()
     wrk.headers["Connection"] = "Keep-Alive"
 
     -- wrk.addr = addrs[math.random(#addrs)]
+    -- for i,v in ipairs(addrs) do print(i,v) end  -- print addrs
     -- wrk.thread.addr = addrs[math.random(#addrs)]
+
+    -- Keep track of numRequests
+    numRequests = numRequests + 1
 
 
     return wrk.format(nil, path)
+end
+
+
+-- TODO TESTING
+function response(status, headers, body)
+    numResponses = numResponses + 1
+end
+
+function done(summary, latency, requests)
+    -- Report results for each thread
+    for index, thread in ipairs(threads) do
+       local id        = thread:get("id")
+       local requests  = thread:get("numRequests")
+       local responses = thread:get("numResponses")
+       local msg = "thread %d made %d requests and got %d responses"
+       print(msg:format(id, requests, responses))
+    end
 end
